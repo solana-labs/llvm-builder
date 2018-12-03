0e30b57fe5ec8b50233dac31758b86d1f185bb80
Jack May <jack@solana.com>
2018-12-02 20:59:58 -0800

    Reduce size, rename to osx

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ mkdir -p deploy
 cp linux/out/solana-llvm-linux.tar.bz2 deploy
 
 if [ "$(uname)" == "Darwin" ]; then
-  (cd macos && ./build.sh )
-  cp macos/out/solana-llvm-macos.tar.bz2 deploy
+  (cd osx && ./build.sh )
+  cp osx/out/solana-llvm-osx.tar.bz2 deploy
 else
-  echo Warning: Local machine not a Mac, cannot build macos variant, skipping
+  echo Warning: Local machine not a Mac, cannot build osx variant, skipping
 fi
 
 
